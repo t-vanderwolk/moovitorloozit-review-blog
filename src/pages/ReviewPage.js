1275e8e7f5849b22110a0a8918eeb32d0fa59fb8
Taylor Vanderwolk <taylorvanderwolk@Taylors-MacBook-Pro.local>
2022-09-08 15:09:26 -0700

    added inProgress page

--- src/pages/ReviewPage.js
+++ src/pages/ReviewPage.js
@@ -4,6 +4,8 @@ import React from 'react'
 
 export default function ReviewPage() {
   return (
-    <h1>Review Page</h1>
+    <>
+    <img  style={{ width:"100%",height:"100%", padding: "2%",marginTop:"5%", marginBottom:"5%" }} className="inProgress" src={require('../pictures/inProgress2.jpeg')} alt="inProgress" /> 
+    </>
   )
 }

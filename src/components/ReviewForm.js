69968bd8e2503cd5aa763ae0ab8c8de2c832787e
emilymorgenbesser <emorgenbesser@sigmasolve.net>
2023-01-11 15:43:17 -0500

    fixed stars for reviews

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -1,20 +1,20 @@
 import React, { useState } from 'react';
 import { Form, Button, Container, Alert } from 'react-bootstrap';
-import '../style/Review.css'
+import '../style/Review.css';
+import StarRating from './StarRating';
 
 const ReviewForm = () => {
+  // <h2>Leave us a Revie</h2>;
 
-    <h2>Leave us a Review</h2>
-    
   const [enteredRating, setRating] = useState('');
   const [enteredName, setName] = useState('');
   const [enteredLocation, setLocation] = useState('');
   const [enteredComment, setComment] = useState('');
   const [enteredDate, setDate] = useState('');
 
-const ratingChangeHandler = (event) => {
+  const ratingChangeHandler = (event) => {
     setRating(event.target.value);
-}
+  };
   const nameChangeHandler = (event) => {
     setName(event.target.value);
   };
@@ -29,57 +29,104 @@ const ratingChangeHandler = (event) => {
 
   const dateChangeHandler = (event) => {
     setDate(event.target.value);
-  }
-
+  };
 
   const submitHandler = (event) => {
     event.preventDefault();
 
     //reset the values of input fields
-        setRating('');
-        setName('');
-        setLocation('');
-        setComment('');
-        setDate('');
+    setRating('');
+    setName('');
+    setLocation('');
+    setComment('');
+    setDate('');
 
-    return alert('Entered Values are:'+enteredRating+','+enteredName+','+ enteredLocation +','+enteredComment +","+enteredDate)
-
-
+    return alert(
+      'Entered Values are:' +
+        enteredRating +
+        ',' +
+        enteredName +
+        ',' +
+        enteredLocation +
+        ',' +
+        enteredComment +
+        ',' +
+        enteredDate
+    );
   };
 
-    return(
-      <Alert  style={{marginLeft:"20%",marginRight:"20%",marginTop:"2%", marginBottom:"2%",backgroundColor:"white",border: "3px solid black"}} variant='primary'>
-      <Container >
-        <h2 style={{textAlign:"center"}}>Leave us a Review</h2>
-      <Form onSubmit={submitHandler}>
-      <Form.Group  controlId="form.rating">
+  return (
+    <Alert
+      style={{
+        marginLeft: '20%',
+        marginRight: '20%',
+        marginTop: '2%',
+        marginBottom: '2%',
+        backgroundColor: 'white',
+        border: '3px solid black',
+      }}
+      variant="primary"
+    >
+      <Container>
+        <h2 style={{ textAlign: 'center' }}>Leave us a Review</h2>
+        <Form onSubmit={submitHandler}>
+          <Form.Group controlId="form.rating">
             <Form.Label>Rating</Form.Label>
-            
-            <Form.Control type="number"  value={enteredRating} onChange={ratingChangeHandler}placeholder="How Was Our Service" required/>
-        </Form.Group>
+            <StarRating />
+            {/* <Form.Control
+              type="number"
+              value={enteredRating}
+              onChange={ratingChangeHandler}
+              placeholder="How Was Our Service"
+              required */}
+            {/* /> */}
+          </Form.Group>
 
-      <Form.Group  controlId="form.name">
+          <Form.Group controlId="form.name">
             <Form.Label>Name</Form.Label>
-            <Form.Control type="text" value={enteredName} onChange={nameChangeHandler}placeholder="Enter Name" required/>
-        </Form.Group>
-        <Form.Group controlId="form.location">
+            <Form.Control
+              type="text"
+              value={enteredName}
+              onChange={nameChangeHandler}
+              placeholder="Enter Name"
+              required
+            />
+          </Form.Group>
+          <Form.Group controlId="form.location">
             <Form.Label>Location</Form.Label>
-            <Form.Control type="text" value={enteredLocation} onChange={locationChangeHandler} placeholder="Enter Location" required/>
-        </Form.Group>
-        <Form.Group  controlId="form.comment">
+            <Form.Control
+              type="text"
+              value={enteredLocation}
+              onChange={locationChangeHandler}
+              placeholder="Enter Location"
+              required
+            />
+          </Form.Group>
+          <Form.Group controlId="form.comment">
             <Form.Label>Comment</Form.Label>
-            <Form.Control type="text" value={enteredComment} onChange={commentChangeHandler} placeholder="Enter Comment" required/>
-        </Form.Group>
-        <Form.Group  controlId="form.date">
+            <Form.Control
+              type="text"
+              value={enteredComment}
+              onChange={commentChangeHandler}
+              placeholder="Enter Comment"
+              required
+            />
+          </Form.Group>
+          <Form.Group controlId="form.date">
             <Form.Label>Date</Form.Label>
-            <Form.Control type="date" value={enteredDate} onChange={dateChangeHandler}placeholder="Enter Date" required/>
-        </Form.Group>
+            <Form.Control
+              type="date"
+              value={enteredDate}
+              onChange={dateChangeHandler}
+              placeholder="Enter Date"
+              required
+            />
+          </Form.Group>
 
-        <Button type='submit'>Submit Review</Button>
-      </Form>
-    </Container>
+          <Button type="submit">Submit Review</Button>
+        </Form>
+      </Container>
     </Alert>
-
-    );
-}
+  );
+};
 export default ReviewForm;

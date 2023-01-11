5bb0de0eb20390c59b60eaba5a2520a131db155f
Taylor Vanderwolk <taylorvanderwolk@Taylors-MacBook-Pro.local>
2023-01-11 12:47:04 -0700

    fixed input field

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -1,22 +1,20 @@
 import React, { useState } from 'react';
 import { Form, Button, Container, Alert } from 'react-bootstrap';
 import '../style/Review.css'
-import RatingStars from './RatingStars';
-import Star from './Star';
 
- const ReviewForm = () => {
+const ReviewForm = () => {
 
     <h2>Leave us a Review</h2>
     
-  // const [enteredRating, setRating] = useState('');
+  const [enteredRating, setRating] = useState('');
   const [enteredName, setName] = useState('');
   const [enteredLocation, setLocation] = useState('');
   const [enteredComment, setComment] = useState('');
   const [enteredDate, setDate] = useState('');
 
-// const ratingChangeHandler = (event) => {
-//     setRating(event.target.value);
-// }
+const ratingChangeHandler = (event) => {
+    setRating(event.target.value);
+}
   const nameChangeHandler = (event) => {
     setName(event.target.value);
   };
@@ -38,17 +36,17 @@ import Star from './Star';
     event.preventDefault();
 
     //reset the values of input fields
-        // setRating('');
+        setRating('');
         setName('');
         setLocation('');
         setComment('');
         setDate('');
 
-    return alert('Entered Values are:'+enteredName+','+ enteredLocation +','+enteredComment +","+enteredDate)
+    return alert('Entered Values are:'+enteredRating+','+enteredName+','+ enteredLocation +','+enteredComment +","+enteredDate)
 
 
   };
-const Star =(props) => {
+
     return(
       <Alert  style={{marginLeft:"20%",marginRight:"20%",marginTop:"2%", marginBottom:"2%",backgroundColor:"white",border: "3px solid black"}} variant='primary'>
       <Container >
@@ -56,28 +54,8 @@ const Star =(props) => {
       <Form onSubmit={submitHandler}>
       <Form.Group  controlId="form.rating">
             <Form.Label>Rating</Form.Label>
-            <label className="star">
-            <input
-                type="radio"
-                name="rating"
-                id={props.grade}
-                value={props.index}
-                className="stars_radio-input"
-            />
-            <svg 
-                width="58" 
-                height="58" 
-                viewBox="0 0 24 24" 
-                fill="none" 
-                stroke="#393939" 
-                strokeWidth="1" 
-                strokeLinecap="round" 
-                strokeLinejoin="round" 
-            >
-                <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon>
-            </svg>
-        </label>
-            {/* <Form.Control type="number"  value={enteredRating} onChange={ratingChangeHandler}placeholder="How Was Our Service" required/> */}
+            
+            <Form.Control type="number"  value={enteredRating} onChange={ratingChangeHandler}placeholder="How Was Our Service" required/>
         </Form.Group>
 
       <Form.Group  controlId="form.name">
@@ -103,5 +81,5 @@ const Star =(props) => {
     </Alert>
 
     );
-    }}
-    export default ReviewForm
+}
+export default ReviewForm;
